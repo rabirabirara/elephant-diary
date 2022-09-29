27b3989a44165cf9881aceed25f74750926e4918
Spencer Gouw <swyverng55@g.ucla.edu>
2022-09-29 03:25:11 -0700

    cleanup, renaming, some notes

--- src/loop.rs
+++
@@ -1,3 +0,0 @@
-
-// loop: open a file for editing.
-// edit: you can see the file as it is, the entire history of the file and all its messages.
--- src/wrap.rs
+++
@@ -1,7 +0,0 @@
-use unicode_width::UnicodeWidthStr;
-use unicode_linebreak::{linebreaks, BreakOpportunity::{Mandatory, Allowed}};
-
-pub fn greedy_wrap(text: String, width: u32) {
-    let v = linebreaks(&text);
-    
-}
